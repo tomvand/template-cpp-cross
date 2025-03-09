10b340241826b97f7832efe6d3f7342e45fffe83
Tom van Dijk <tomvand@users.noreply.github.com>
2025-03-09 10:33:19 +0100

    Initial commit

---
+++ src/native/main.cpp
@@ -0,0 +1,12 @@
+// put function declarations here:
+int myFunction(int, int);
+
+int main(int argc, char **argv) {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
